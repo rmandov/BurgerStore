85d645d1cfbf305678f3eb97ad4936714022ea05
Armando Velasco <armando.blye@gmail.com>
2021-05-31 15:56:50 -0500

    New form for structs
    I've added new form to work with structs

--- main.c
+++ main.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 5
+
 
 // Structure for burgers
-struct Burger {
-	char name[20];
+struct burger {
+	const char *name;
 	int number;
 	float price;
 };
+typedef struct burger Burger;
 
-void show(struct Burger B[]) {
-	printf("\t\tCOLOR - BURGERS\n");
+void show(const Burger * const B) {
+	printf("\t**** COLOR - BURGERS ****\n");
 	printf("//Burger\t//Cantidad Disponible\t//Precio\n\n");
 
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
-		printf("%i. %s\t\t%i\t\t$%.2f\n", i + 1, (B+i)->name, (B+i)->number, (B + i)->price);
+		printf("%i. %s\t\t%i\t\t$%.2f\n", i + 1, B[i].name, B[i].number, B[i].price);
 	}
 }
 
-void fillStore(struct Burger B[], char* C[], int stop) {
+void fillStore(Burger * const B,const char * C[]) {
 	int azar = 2;
 
-	for (int i = 0; i < stop; i++) {
-		(B + i)->number = azar * 5;
-		(B + i)->price = azar * 13.25;
-		strcpy((B+i)->name, *(C + i));
+	for (size_t i = 0; i < SIZE; i++) {
+		B[i].name = C[i];
+		B[i].number = azar * 5;
+		B[i].price = 13.5 + i;
 		azar++;
 	}
 }
@@ -33,15 +36,15 @@ void fillStore(struct Burger B[], char* C[], int stop) {
 int main() {
 
 	// Create the burgers for my store and structs for store and shopping cart.
-	char* colors[] = { "Black", "Green", "Yellow", "Blue ", "Orange", "Armando" };
-	// Define the number of burgers I have
-	int numberColors = (int)(sizeof(colors) / sizeof(colors[0]));
-	printf("%i", numberColors);
-	struct Burger newBurgers[numberColors];
-	// struct Burger shoppingCart[5];
+	const char* name[] = { "Black", "Green", "Yellow", "Blue ", "Orange"};
+	
+	
+	Burger newBurgers[SIZE];
 
 	// Fill store with burgers.
-	fillStore(newBurgers,colors, numberColors);
+	fillStore(newBurgers,name);
+
+
 
 	show(newBurgers);
 
